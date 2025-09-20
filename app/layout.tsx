e9c9892dd51ecebae75f406571c6667b5d4d3347
fkumdc-stack <fkumdc@gmail.com>
2025-09-20 13:35:13 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Dignity Dukaan',
+  description: 'Created with Love By Dignity Owners',
   generator: 'v0.app',
 }
 
